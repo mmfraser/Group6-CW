e79d3d4ab6cd39dd3c2da7c2220cffdb63242d54
Marc Fraser <marc@coopersoftware.co.uk>
2011-10-27 21:46:52 +0100

    Creating a page class to control the app's pages

---
+++ AppClasses/page.php
@@ -0,0 +1,23 @@
+<?php
+	
+	class Page {
+		public $title;
+		public function getHeader() {
+			$output = '<!DOCTYPE html>' . PHP_EOL;
+			$output .= '	<head>' . PHP_EOL;
+			$output .= '		<title>' . $this->title . '</title>' . PHP_EOL;
+			$output .= '		<link type="text/css" href="../css/themename/jquery-ui-1.8.16.custom.css" rel="Stylesheet" />' . PHP_EOL;
+			$output .= '		<script type="text/javascript" src="../js/jquery-1.4.4.min.js"></script>' . PHP_EOL;
+			$output .= '		<script type="text/javascript" src="../js/jquery-ui-1.8.16.custom.min.js"></script>' . PHP_EOL;
+			$output .= "	</head>" . PHP_EOL;
+			
+			echo $output;
+		}
+		
+		public function getFooter() {
+			
+		}		
+		
+	}
+	
+?>
