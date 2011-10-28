feb3a0d7ca571c3bf0d371156ad98ebcc207ef80
Marc Fraser <marc@coopersoftware.co.uk>
2011-10-28 12:22:39 +0100

    adding footer html

--- AppClasses/page.php
+++ AppClasses/page.php
@@ -10,12 +10,14 @@
 			$output .= '		<script type="text/javascript" src="../js/jquery-1.4.4.min.js"></script>' . PHP_EOL;
 			$output .= '		<script type="text/javascript" src="../js/jquery-ui-1.8.16.custom.min.js"></script>' . PHP_EOL;
 			$output .= "	</head>" . PHP_EOL;
-			
+			$output .= "	<body>" .PHP_EOL;
 			echo $output;
 		}
 		
 		public function getFooter() {
-			
+			$output = "		</body>";
+			$output .= "</html>";
+			echo $output;
 		}		
 		
 	}
