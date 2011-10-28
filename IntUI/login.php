ff60880a65551d38b0514885be4aa10664f9554f
Marc Fraser <marc@coopersoftware.co.uk>
2011-10-28 14:16:02 +0100

    minor fixes

--- IntUI/login.php
+++ IntUI/login.php
@@ -3,12 +3,21 @@
 	$page = new Page();
 	$page->title = "test";
 	$page->getHeader();
-	?>
+?>
+	<script>
 	
-	My form goes here.
-	
-	
-<?php	$page->getFooter();
-	
-	
+	$(function() {
+		$( "input:submit, a, button").button();
+	//	$( "a", ".demo" ).click(function() { return false; });
+	});
+	</script>
+<form method="POST" action="?do=login">
+  Username: <input type="text" name="username" size="15" /><br />
+  Password: <input type="password" name="passwort" size="15" /><br />
+  <input type="submit" value="Login" />
+</form>
+
+
+<?php	
+	$page->getFooter();
 ?>
