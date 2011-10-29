f3a015dbd1d7d919da1c5d660703b878631e9db8
Marc Fraser <marc@coopersoftware.co.uk>
2011-10-29 15:48:51 +0100

    Moving App.php and adding new JS to page.php.

--- AppClasses/page.php
+++ AppClasses/page.php
@@ -2,13 +2,19 @@
 	
 	class Page {
 		public $title;
+		private $headJS = "";
+		
 		public function getHeader() {
 			$output = '<!DOCTYPE html>' . PHP_EOL;
 			$output .= '	<head>' . PHP_EOL;
 			$output .= '		<title>' . $this->title . '</title>' . PHP_EOL;
+			$output .= '		<link type="text/css" href="../css/IntUI.css" rel="Stylesheet" />' . PHP_EOL;
 			$output .= '		<link type="text/css" href="../css/smoothness/jquery-ui-1.8.16.custom.css" rel="Stylesheet" />' . PHP_EOL;
+			$output .= '		<link type="text/css" href="../css/datatable/demo_table_jui.css" rel="Stylesheet" />' . PHP_EOL;
 			$output .= '		<script type="text/javascript" src="../js/jquery-1.6.2.min.js"></script>' . PHP_EOL;
 			$output .= '		<script type="text/javascript" src="../js/jquery-ui-1.8.16.custom.min.js"></script>' . PHP_EOL; 	
+			$output .= '		<script type="text/javascript" src="../js/jquery.dataTables.min.js"></script>' . PHP_EOL;
+			$output .= '		<script type="text/javascript">' . $this->headJS . '</script>';
 			$output .= '	</head>' . PHP_EOL;
 			$output .= '	<body>' .PHP_EOL;
 			
@@ -19,7 +25,11 @@
 			$output = '		</body>';
 			$output .= '</html>';
 			echo $output;
-		}		
+		}	
+
+		public function addHeadJS($js) {
+			$this->headJS .= $js;
+		}
 		
 	}
 	
