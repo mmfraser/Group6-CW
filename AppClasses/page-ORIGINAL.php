1de7b31cc75fd1994cda769e94110bbb44b228c9
David Turner <dvdturner91@gmail.com>
2012-01-24 13:26:26 +0000

    Uploading style for IntUI

--- AppClasses/page-ORIGINAL.php
+++ AppClasses/page-ORIGINAL.php
@@ -17,7 +17,6 @@
 			$output .= '	<head>' . PHP_EOL;
 			$output .= '		<title>' . $this->title . '</title>' . PHP_EOL;
 			$output .= '		<link type="text/css" href="../css/IntUI.css" rel="Stylesheet" />' . PHP_EOL;
-			$output .= '		<link type="text/css" href="../css/Navigation.css" rel="Stylesheet" />' . PHP_EOL;
 			$output .= '		<link type="text/css" href="../css/smoothness/jquery-ui-1.8.16.custom.css" rel="Stylesheet" />' . PHP_EOL;
 			$output .= '		<link type="text/css" href="../css/datatable/demo_table_jui.css" rel="Stylesheet" />' . PHP_EOL;
 			$output .= '		<script type="text/javascript" src="../js/jquery-1.6.2.min.js"></script>' . PHP_EOL;
@@ -39,56 +38,12 @@
 		
 		private function header() { 
 			$output = '<div id="header">'. PHP_EOL;
-			//$output .= '	Quicksilver Music'. PHP_EOL;
-			$output .= '<img src="../Images/logo4.png" alt="Quicksilver Music" />';
+			$output .= '	Test'. PHP_EOL;
 			$output .='</div>'. PHP_EOL;
 			return $output;
 		}
 		
 		private function navigation() {
-			if(App::checkAuth()) {
-					$output = '<div style="margin-left:auto; margin-right:auto; width:100%;">' .PHP_EOL;
-					$output .= '<div class="menu" id="navigation">' .PHP_EOL;
-					$output .= '<ul>' .PHP_EOL;
-					$output .= '<li><a href="#">Dashboard<!--[if gte IE 7]><!--></a><!--<![endif]--></li>' .PHP_EOL;
-					$output .= '<li><a href="#">Chart Management<!--[if gte IE 7]><!--></a><!--<![endif]--></li>' .PHP_EOL;
-					$output .= '<li><a href="#">Management<!--[if gte IE 7]><!--></a><!--<![endif]-->' .PHP_EOL;
-					$output .= '<!--[if lte IE 6]><table><tr><td><![endif]-->' .PHP_EOL;
-					$output .= '	<ul>' .PHP_EOL;
-					$output .= '		<li><a href="userManagement.php">User Management</a></li>' .PHP_EOL;
-					$output .= '        <li><a href="storeManagement.php">Store Management</a></li>' .PHP_EOL;
-					$output .= '        <li><a href="genreManagement.php">Genre Management</a></li>' .PHP_EOL;
-					$output .= '	</ul>' .PHP_EOL;
-					$output .= '<!--[if lte IE 6]></td></tr></table></a><![endif]-->' .PHP_EOL;
-					$output .= '</li>' .PHP_EOL;
-					$output .= '<li><a href="#">Data Import<!--[if gte IE 7]><!--></a><!--<![endif]-->' .PHP_EOL;
-					$output .= '<!--[if lte IE 6]><table><tr><td><![endif]-->' .PHP_EOL;
-					$output .= '	<ul>' .PHP_EOL;
-					$output .= '	<!--	<li><a href="">Import Overview</a></li> -->' .PHP_EOL;
-					$output .= '        <li><a href="ArtistImport.php">Artist Import</a></li>' .PHP_EOL;
-					$output .= '        <li><a href="ProductImport.php">Products Import</a></li>' .PHP_EOL;
-					$output .= '        <li><a href="SalesImport.php">Sales Import</a></li>' .PHP_EOL;
-					$output .= '	</ul>' .PHP_EOL;					
-					$output .= '<!--[if lte IE 6]></td></tr></table></a><![endif]-->' .PHP_EOL;
-					$output .= '</li>' .PHP_EOL;
-					$output .= '<li><a href="#">Data Management<!--[if gte IE 7]><!--></a><!--<![endif]-->' .PHP_EOL;
-					$output .= '<!--[if lte IE 6]><table><tr><td><![endif]-->' .PHP_EOL;
-					$output .= '	<ul>' .PHP_EOL;
-					$output .= '		<li><a href="artistManagement.php">Artist Management</a></li>' .PHP_EOL;
-					$output .= '        <li><a href="productManagement.php">Product Management</a></li>' .PHP_EOL;
-					$output .= '        <li><a href="salesManagement.php">Sales Management</a></li>' .PHP_EOL;
-					$output .= '</ul>' .PHP_EOL;
-					$output .= '<li><a href="login.php?do=logout">Logout<!--[if gte IE 7]><!--></a><!--<![endif]--></li>' .PHP_EOL;
-					$output .='</div>' .PHP_EOL;
-					$output .='</div>' .PHP_EOL;
-			} else {
-				// User is not logged in therefore no need to see the navigation.
-				$output = "";
-			}
-			return $output;
-		}
-		
-		/*private function navigation2() {
 			if(App::checkAuth()) {
 				$output = '		<div id="navigation">' .PHP_EOL;
 				$output .= '		<ul>'.PHP_EOL;
@@ -121,7 +76,7 @@
 				$output = "";
 			}
 			return $output;
-		}*/
+		}
 
 		private function footerContent() {
 			$output = '		<div class="clear"></div>'.PHP_EOL;
